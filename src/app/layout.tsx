39e38cc5065b197e358f5854e981428a9822b7c8
brenokf <breno-kf@hotmail.com>
2025-07-14 23:29:47 -0400

    add loading and function active loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,33 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { GlobalStyle } from '../styles/global-styles';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 import { Header } from '../components/Header';
+import AtomLoadingScreen from '../components/LoadingScreen';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const simulateProgress = () => {
+      let current = 0;
+      const interval = setInterval(() => {
+        current += Math.random() * 20;
+        if (current >= 100) {
+          current = 100;
+          clearInterval(interval);
+          setTimeout(() => setLoading(false), 500);
+        }
+        setProgress(current);
+      }, 200);
+    };
+
+    simulateProgress();
+  }, []);
+
   return (
     <html lang="pt-BR">
       <head>
@@ -15,6 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
+          {loading && <AtomLoadingScreen progress={progress} />}
           <Header />
           {children}
         </ThemeProvider>
